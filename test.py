57e9a1dfd9eef25ebaf91ea5adc0898f26ca87fd
Devon Schwartz <41024725+DevonSchwartz@users.noreply.github.com>
2021-06-10 19:21:05 +0000

    Update Doc

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-from states import states
-print (states.getDF())
-texasIndexes = states.getStateIndex("Texas")
-dates = states.getDateRange("2021-01-21", "2021-02-21")
+from states import states 
 
 
-states.graphDatesDeaths("Texas",dates)
+print (states.getDF()) 
+
+
+lst = states.getDateRange("2021-05-01","2021-05-25") 
+datesBegin = states.getDateIndex("2021-05-01")
+datesEnd = states.getDateIndex("2021-05-24")
+start = 0
+end = 0 
+for i in datesBegin:
+	if states.getState(i) == "Texas" :
+		start = i 
+		break 
+
+for i in datesEnd:
+	if states.getState(i) == "Texas" :
+		end = i + 1
+		break 
+
+print (start,end)
+
+for i in range(start,end):
+	if (states.getState(i) == "Texas"):
+		print (i, states.getState(i), states.getDate(i), str(states.getDailyCaseCount(i))) 
